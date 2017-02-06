b999c3ac99fb44b1943eb3a2be7bf4cd7f5770f4
Jorge Alferez Adan <>
2017-02-06 16:07:59 +0100

    Version inicial

---
+++ public_html/contabilidad/plugins/paneldeclientes/controller/paneldeclientes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * @author MERCAINFORMATICA      comercial@mercainformatica.es
+ * @copyright 2017, REPARACION Y MANTENIMIENTO INFORMATICO MADRID SL. All Rights Reserved.
+ */
+
+
+/**
+ * Description of PANEL DE CLIENTES
+ *
+ * @author MERCAINFORMATICA
+ */
+class paneldeclientes extends fs_controller
+{
+   public $texto;
+   public $texto2;
+   public $lista;
+   public $resultados_sql;
+   
+   public function __construct()
+   {
+      parent::__construct(__CLASS__, 'Mis Servicios', 'Panel Cliente');
+   }
+   
+   protected function private_core()
+   {
+      $this->texto = 'hola mundo';
+      $this->texto2 = 'Bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla.';
+      $this->lista = array('peras', 'manzanas', 'puerros', 'naranjas');
+      
+      $this->resultados_sql = $this->db->select("select * from servicioscli;");
+   }
+}
